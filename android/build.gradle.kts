da8f70994dfb3b6893d672b5a53ba782f3fe513e
sooubh <sourabh3527@gmail.com>
2026-05-09 22:40:05 +0530

    uodate7

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,13 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    afterEvaluate {
+        if (project.name == "isar_flutter_libs") {
+            val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            android?.namespace = "dev.isar.isar_flutter_libs"
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
