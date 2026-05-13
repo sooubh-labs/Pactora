578bb3bf0ebc13345b095889758d8de1acdc064d
sooubh <sourabh3527@gmail.com>
2026-05-13 21:47:37 +0530

    feat: create reusable BannerAdWidget

---
+++ lib/core/ads/banner_ad_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import '../ads/ad_constants.dart';
+import '../providers/user_preferences_provider.dart';
+
+class BannerAdWidget extends ConsumerStatefulWidget {
+  const BannerAdWidget({super.key});
+
+  @override
+  ConsumerState<BannerAdWidget> createState() => _BannerAdWidgetState();
+}
+
+class _BannerAdWidgetState extends ConsumerState<BannerAdWidget> {
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAd();
+  }
+
+  void _loadAd() {
+    final isPremium = ref.read(isPremiumProvider);
+    if (isPremium) return;
+
+    _bannerAd = BannerAd(
+      adUnitId: AdConstants.bannerAdUnitId,
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          ad.dispose();
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isPremium = ref.watch(isPremiumProvider);
+
+    if (isPremium) {
+      return const SizedBox.shrink();
+    }
+
+    if (_isAdLoaded && _bannerAd != null) {
+      return Container(
+        alignment: Alignment.center,
+        width: _bannerAd!.size.width.toDouble(),
+        height: _bannerAd!.size.height.toDouble(),
+        child: AdWidget(ad: _bannerAd!),
+      );
+    }
+
+    return const SizedBox(
+      height: 50,
+      child: Center(
+        child: CircularProgressIndicator.adaptive(),
+      ),
+    );
+  }
+}
